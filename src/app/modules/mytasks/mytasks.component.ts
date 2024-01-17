7b941cb843635bf641f9557fd9f8a65220c5be6a
Josua Xiao <Yunfan.xiao@proleit.com>
2024-01-17 13:42:34 +0100

    MyTasks funtzt

--- src/app/modules/mytasks/mytasks.component.ts
+++ src/app/modules/mytasks/mytasks.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../user/user.service';
+import { User } from '../../user/user';
+import { KanbanItem } from '../../services/KanbanItem';
+import { FlosKanbanService } from '../../services/flos-kanban.service';
+import { Subject, takeUntil } from 'rxjs';
+import { KanbanState } from '../../services/kanban-state';
 @Component({
   selector: 'pit-mytasks',
   templateUrl: './mytasks.component.html',
-  styleUrl: './mytasks.component.scss'
+  styleUrl: './mytasks.component.scss',
 })
 export class MytasksComponent {
+  readonly $user: UserService = inject(UserService);
 
+  user: User[] = [];
+  items$ = this.kanbanService.get();
+
+  items: KanbanItem[] = [];
+
+  KanbanState = KanbanState;
+
+  #ngUnsubscribe = new Subject<void>();
+
+  constructor(private kanbanService: FlosKanbanService) {}
+
+  show(status: string, column: string): boolean {
+    console.log('hallo');
+    if (status === column) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  ngOnInit(): void {
+    this.kanbanService
+      .get()
+      .pipe(takeUntil(this.#ngUnsubscribe))
+      .subscribe({
+        next: (value) => {
+          this.items = value;
+          console.log(this.items);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.#ngUnsubscribe.next();
+    this.#ngUnsubscribe.complete();
+  }
 }
